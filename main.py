b2889aa9f9863bc97bd1563e4c2c15bad362f498
ahmed2402 <ahmedraza312682@gmail.com>
2025-03-29 02:21:07 +0500

    fuck deployment

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ genai.configure(api_key=gemini_api_key)
 
 model = genai.GenerativeModel(model_name="gemini-2.0-flash")
 
-# @cl.oauth_callback
-# def oaut_callback(
-#     provider_id : str ,
-#     token : str,
-#     user_data : Dict[str, str],
-#     default_user : cl.User,
-# ) -> Optional[cl.User]:
-#     """
-#     Handle the OAUTH Callback from GITHUB
-#     RETURN THE USER OBJ IF AUTH IS SUCCESSFUL, none otherwise 
-#     """
-#     print(f"Provide : {provider_id}")
-#     print(f"User Data : {user_data}")
+@cl.oauth_callback
+def oaut_callback(
+    provider_id : str ,
+    token : str,
+    user_data : Dict[str, str],
+    default_user : cl.User,
+) -> Optional[cl.User]:
+    """
+    Handle the OAUTH Callback from GITHUB
+    RETURN THE USER OBJ IF AUTH IS SUCCESSFUL, none otherwise 
+    """
+    print(f"Provide : {provider_id}")
+    print(f"User Data : {user_data}")
 
-#     return default_user
+    return default_user
 
 @cl.on_chat_start
 async def handle_chat_start():
@@ -51,7 +51,3 @@ async def handle_message(message: cl.Message):
     cl.user_session.set("history" , history)
 
     await cl.Message(content=response_text).send()
-
-if __name__ == "__main__":
-    import chainlit as cl
-    cl.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
